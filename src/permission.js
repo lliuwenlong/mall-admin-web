1d9e45b9387063c83dab491e0f9591709ca74a70
root <18801295284@163.com>
2019-05-25 15:44:35 +0800

    delete

--- src/permission.js
+++ src/permission.js
@@ -7,25 +7,13 @@ import { getToken } from '@/utils/auth' // 验权
 
 const whiteList = ['/login'] // 不重定向白名单
 router.beforeEach((to, from, next) => {
-  return next();
   NProgress.start()
   if (getToken()) {
     if (to.path === '/login') {
       next({ path: '/' })
       NProgress.done() // if current page is dashboard will not trigger	afterEach hook, so manually handle it
     } else {
-      if (store.getters.roles.length === 0) {
-        store.dispatch('GetInfo').then(res => { // 拉取用户信息
-          next()
-        }).catch((err) => {
-          store.dispatch('FedLogOut').then(() => {
-            Message.error(err || 'Verification failed, please login again')
-            next({ path: '/' })
-          })
-        })
-      } else {
-        next()
-      }
+      next();
     }
   } else {
     if (whiteList.indexOf(to.path) !== -1) {
