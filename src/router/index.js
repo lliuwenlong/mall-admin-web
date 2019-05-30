4ba2ebe1524663df9d2f3b3ebf86940833489b49
root <18801295284@163.com>
2019-05-30 09:36:26 +0800

    添加课程

--- src/router/index.js
+++ src/router/index.js
@@ -48,6 +48,18 @@ export const constantRouterMap = [
             name: 'setBasic',
             component: () => import('@/views/system/setBasic/index'),
             meta: { title: '基础设置', icon: 'xt2' }
+        },
+        {
+            path: 'vipSet',
+            name: 'vipSet',
+            component: () => import('@/views/basicsSetup/vipSet'),
+            meta: { title: 'vip设置', icon: 'product-list' }
+        },
+        {
+            path: 'banner',
+            name: 'banner',
+            component: () => import('@/views/basicsSetup/banner'),
+            meta: { title: 'banner管理', icon: 'product-list' }
         }],
     },
     {
@@ -89,9 +101,7 @@ export const constantRouterMap = [
             component: () => import('@/views/underLine/show'),
             meta: { title: '线下管理展示', icon: 'xianxia' }
         }]
-    }
-    ////////////////////////////////////////////////////
-    ,
+    },
     {
         path: '/user',
         component: Layout,
@@ -109,72 +119,6 @@ export const constantRouterMap = [
     },
     { path: '*', redirect: '/404', hidden: true },
     {
-        path: '/oms',
-        component: Layout,
-        redirect: '/oms/order',
-        name: 'oms',
-        meta: { title: '基础设置', icon: 'order' },
-        children: [
-            {
-                path: 'vipSet',
-                name: 'vipSet',
-                component: () => import('@/views/basicsSetup/vipSet'),
-                meta: { title: 'vip设置', icon: 'product-list' }
-            },
-            {
-                path: 'banner',
-                name: 'banner',
-                component: () => import('@/views/basicsSetup/banner'),
-                meta: { title: 'banner管理', icon: 'product-list' }
-            },
-            {
-                path: 'order',
-                name: 'order',
-                component: () => import('@/views/oms/order/index'),
-                meta: { title: '订单列表', icon: 'product-list' }
-            },
-            {
-                path: 'orderDetail',
-                name: 'orderDetail',
-                component: () => import('@/views/oms/order/orderDetail'),
-                meta: { title: '订单详情' },
-                hidden: true
-            },
-            {
-                path: 'deliverOrderList',
-                name: 'deliverOrderList',
-                component: () => import('@/views/oms/order/deliverOrderList'),
-                meta: { title: '发货列表' },
-                hidden: true
-            },
-            {
-                path: 'orderSetting',
-                name: 'orderSetting',
-                component: () => import('@/views/oms/order/setting'),
-                meta: { title: '订单设置', icon: 'order-setting' }
-            },
-            {
-                path: 'returnApply',
-                name: 'returnApply',
-                component: () => import('@/views/oms/apply/index'),
-                meta: { title: '退货申请处理', icon: 'order-return' }
-            },
-            {
-                path: 'returnReason',
-                name: 'returnReason',
-                component: () => import('@/views/oms/apply/reason'),
-                meta: { title: '退货原因设置', icon: 'order-return-reason' }
-            },
-            {
-                path: 'returnApplyDetail',
-                name: 'returnApplyDetail',
-                component: () => import('@/views/oms/apply/applyDetail'),
-                meta: { title: '退货原因详情' },
-                hidden: true
-            }
-        ]
-    },
-    {
         path: '/courseManagement',
         component: Layout,
         redirect: '/sms/coupon',
@@ -188,89 +132,10 @@ export const constantRouterMap = [
                 meta: { title: '课程列表', icon: 'sms-flash' }
             },
             {
-                path: 'flashSession',
-                name: 'flashSession',
-                component: () => import('@/views/sms/flash/sessionList'),
-                meta: { title: '秒杀时间段列表' },
-                hidden: true
-            },
-            {
-                path: 'selectSession',
-                name: 'selectSession',
-                component: () => import('@/views/sms/flash/selectSessionList'),
-                meta: { title: '秒杀时间段选择' },
-                hidden: true
-            },
-            {
-                path: 'flashProductRelation',
-                name: 'flashProductRelation',
-                component: () => import('@/views/sms/flash/productRelationList'),
-                meta: { title: '秒杀商品列表' },
-                hidden: true
-            },
-            {
-                path: 'coupon',
-                name: 'coupon',
-                component: () => import('@/views/sms/coupon/index'),
-                meta: { title: '优惠券列表', icon: 'sms-coupon' }
-            },
-            {
-                path: 'addCoupon',
-                name: 'addCoupon',
-                component: () => import('@/views/sms/coupon/add'),
-                meta: { title: '添加优惠券' },
-                hidden: true
-            },
-            {
-                path: 'updateCoupon',
-                name: 'updateCoupon',
-                component: () => import('@/views/sms/coupon/update'),
-                meta: { title: '修改优惠券' },
-                hidden: true
-            },
-            {
-                path: 'couponHistory',
-                name: 'couponHistory',
-                component: () => import('@/views/sms/coupon/history'),
-                meta: { title: '优惠券领取详情' },
-                hidden: true
-            },
-            {
-                path: 'brand',
-                name: 'homeBrand',
-                component: () => import('@/views/sms/brand/index'),
-                meta: { title: '品牌推荐', icon: 'product-brand' }
-            },
-            {
-                path: 'new',
-                name: 'homeNew',
-                component: () => import('@/views/sms/new/index'),
-                meta: { title: '新品推荐', icon: 'sms-new' }
-            },
-            {
-                path: 'hot',
-                name: 'homeHot',
-                component: () => import('@/views/sms/hot/index'),
-                meta: { title: '人气推荐', icon: 'sms-hot' }
-            },
-            {
-                path: 'subject',
-                name: 'homeSubject',
-                component: () => import('@/views/sms/subject/index'),
-                meta: { title: '专题推荐', icon: 'sms-subject' }
-            },
-            {
-                path: 'advertise',
-                name: 'homeAdvertise',
-                component: () => import('@/views/sms/advertise/index'),
-                meta: { title: '广告列表', icon: 'sms-ad' }
-            },
-            {
-                path: 'addAdvertise',
-                name: 'addHomeAdvertise',
-                component: () => import('@/views/sms/advertise/add'),
-                meta: { title: '添加广告' },
-                hidden: true
+                path: 'addCourseManagement',
+                name: 'addCourseManagement',
+                component: () => import('@/views/courseManagement/addCourseManagement'),
+                meta: { title: '添加课程', icon: 'sms-flash' }
             }
         ]
     },
